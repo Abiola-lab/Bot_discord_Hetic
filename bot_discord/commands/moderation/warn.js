1b5a8b5534dd08a6e9c137b39a5eb710df4a6ab8
baw351 <72230455+baw351@users.noreply.github.com>
2025-02-16 20:06:29 +0100

    commentary warn.js

--- bot_discord/commands/moderation/warn.js
+++ bot_discord/commands/moderation/warn.js
@@ -3,30 +3,37 @@ const { addWarning, getWarnings } = require('../../services/warningsService');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('warn')
-        .setDescription("Donne un avertissement à un utilisateur")
+        .setName('warn')  // Command name is 'warn'
+        .setDescription("Gives a warning to a user")  // Short description of the command
         .addUserOption(option =>
-            option.setName('utilisateur')
-                .setDescription("L'utilisateur à avertir")
-                .setRequired(true))
+            option.setName('utilisateur')  // Option to select a user to warn
+                .setDescription("The user to warn")  // Describes this option
+                .setRequired(true))  // Makes this option mandatory
         .addStringOption(option =>
-            option.setName('raison')
-                .setDescription("Raison de l'avertissement")
-                .setRequired(true)),
+            option.setName('raison')  // Option to add a reason for the warning
+                .setDescription("The reason for the warning")  // Describes this option
+                .setRequired(true)),  // Makes this option mandatory
 
     async execute(interaction) {
-        const user = interaction.options.getUser('utilisateur');
-        const reason = interaction.options.getString('raison');
-        const guildId = interaction.guild.id;
+        const user = interaction.options.getUser('utilisateur');  // Get the selected user
+        const reason = interaction.options.getString('raison');  // Get the reason for the warning
+        const guildId = interaction.guild.id;  // Get the guild ID (server)
 
+        // Check if the executing member has permission to warn users
         if (!interaction.member.permissions.has('KICK_MEMBERS')) {
-            return interaction.reply({ content: "❌ Vous n'avez pas la permission d'avertir un membre.", ephemeral: true });
+            return interaction.reply({ content: "❌ You do not have permission to warn members.", ephemeral: true });
         }
 
+        // Add a warning to the user
         await addWarning(user.id, guildId, reason);
+
+        // Get the total number of warnings for the user
         const warningCount = await getWarnings(user.id, guildId);
 
-        await interaction.reply(`⚠️ **${user.tag}** a été averti pour : **${reason}** (Total: ${warningCount} warnings)`);
-        await user.send(`🚨 **Avertissement sur ${interaction.guild.name}**\nRaison: ${reason}\nTotal warnings: ${warningCount}`);
+        // Respond to the interaction with the details of the warning
+        await interaction.reply(`⚠️ **${user.tag}** has been warned for: **${reason}** (Total: ${warningCount} warnings)`);
+
+        // Send a DM to the user notifying them of the warning
+        await user.send(`🚨 **Warning on ${interaction.guild.name}**\nReason: ${reason}\nTotal warnings: ${warningCount}`);
     }
 };
